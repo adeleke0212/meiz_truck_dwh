e172f2bd0cc084f4a9660a848a2ccf4376e2a0b6
adeleke0212 <128915740+adeleke0212@users.noreply.github.com>
2023-09-28 01:42:21 +0300

    transformed datasets

--- transformation.py
+++ transformation.py
@@ -10,28 +10,10 @@ def transformCustomers():
     df['registered_day'] = pd.DatetimeIndex(df['registered_at']).day
     return df
 
-
-def transformCustomers():
-    df = pd.read_csv('Datasets/customers.csv')
-    df['registered_at'] = pd.to_datetime(df['registered_at'])
-    df['ConvertedDate'] = df['registered_at'].astype(str)
-    df['registered_year'] = pd.DatetimeIndex(df['registered_at']).year
-    df['registered_month'] = pd.DatetimeIndex(df['registered_at']).month
-    df['registered_day'] = pd.DatetimeIndex(df['registered_at']).day
-    return df
-
-# slicer = lambda datetime_text: datetime_text[11:13]
+# # slicer = lambda datetime_text: datetime_text[11:13]
 
 
 def slicer(datetime_text): return datetime_text[11:13]
-
-
-def getHour(df):
-    df['registered_hour'] = df['ConvertedDate'].apply(slicer)
-    df['registered_hour'] = df['registered_hour'].astype(int)
-    df = df[['id', 'name', 'email', 'registered_at', 'registered_year',
-             'registered_month', 'registered_day', 'registered_hour']]
-    df.to_csv('transformed_Datasets/customerstdf.csv')
 
 
 def getHour(df):
@@ -57,7 +39,30 @@ def transformTransactions():
     tdf["quarter"] = tdf.date.dt.quarter
     tdf = tdf[['id', 'customer_id', 'item_id', 'date', 'bank_id', 'qty',
                'transaction_year', 'transaction_month', 'transaction_day', 'quarter']]
-    tdf.to_csv('transformed_Datasets/transactions.csv')
+    tdf.to_csv('transformed_Datasets/transactions.csv', index=False)
 
 
 transformTransactions()
+
+
+def transform_others():
+    banks_df = pd.read_csv('Datasets/banks.csv')
+    banks_df = banks_df.rename(
+        columns={'_id': 'id', 'code': 'bank_code', 'name': 'bank_name'})
+    banks_df = banks_df[['id', 'bank_name', 'bank_code']]
+    banks_df.to_csv('transformed_Datasets/banks.csv', index=False)
+
+    ex_rate_df = pd.read_csv('Datasets/exchange_rates.csv')
+    ex_rate_df = ex_rate_df.rename(
+        columns={'rate': 'exchange_rate'})
+    ex_rate_df = ex_rate_df[['exchange_rate', 'bank_id', 'date']]
+    ex_rate_df.to_csv('transformed_Datasets/exchange_rates.csv', index=False)
+
+    items_df = pd.read_csv('Datasets/items.csv')
+    items_df = items_df.rename(
+        columns={'name': 'truck_name', })
+    items_df = items_df[['id', 'truck_name', 'cost_price', 'selling_price']]
+    items_df.to_csv('transformed_Datasets/items.csv', index=False)
+
+
+transform_others()
